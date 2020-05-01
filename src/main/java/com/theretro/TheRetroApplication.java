83915605d71338b39ab23fe92321916e4c6ae818
Yvan GAKUBA <gakyvan@gmail.com>
2020-05-01 13:29:30 -0400

    initial commit

---
+++ src/main/java/com/theretro/TheRetroApplication.java
@@ -0,0 +1,13 @@
+package com.theretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRetroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRetroApplication.class, args);
+    }
+
+}
